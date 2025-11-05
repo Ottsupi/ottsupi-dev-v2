288bde4c23acf61919728f0ef823e85894842f99
ottsupi <ott.regisorate@gmail.com>
2025-11-05 02:38:10 +0000

    build: add new dependency alpine resize
    also fixed typescript errors by including @types

--- alpine-entrypoint.ts
+++ alpine-entrypoint.ts
@@ -1,6 +1,8 @@
 import type { Alpine } from 'alpinejs'
 import collapse from '@alpinejs/collapse'
+import resize from '@alpinejs/resize'
 
 export default (Alpine: Alpine) => {
     Alpine.plugin(collapse)
+    Alpine.plugin(resize)
 }
